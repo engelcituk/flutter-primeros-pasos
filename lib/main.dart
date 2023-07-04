67eaf339ba64a8413a16b817c3ac4c0e7f56f4f9
cituk <ecituk@thefives.com.mx>
2023-07-03 23:46:52 -0500

    Material design 3

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreen()
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.purpleAccent
+      ),
+      home: const CounterScreen()
     );
   }
 }
